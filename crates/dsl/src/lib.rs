6d3bab4c8efea9e129f3cb73dbbb2deb63f26bb2
yayanyang <yayanyang@gmail.com>
2024-12-12 14:22:35 +0800

    init

--- crates/dsl/src/lib.rs
+++
@@ -1,9 +0,0 @@
-//! Rust dsl for cotati vector graphics language(VGL)
-
-pub mod dsl;
-pub mod generator;
-
-/// The attributes used by graphic elements.
-pub mod attrs {
-    pub use vglang_ir::*;
-}
