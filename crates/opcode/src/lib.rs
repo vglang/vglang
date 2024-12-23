1f5be74b089821a9076257fa9ea6dace1d8d5d2f
yayanyang <yayanyang@gmail.com>
2024-12-23 19:36:37 +0800

    remove all

--- crates/opcode/src/lib.rs
+++ crates/opcode/src/lib.rs
@@ -2,15 +2,3 @@
 //!
 
 #![cfg_attr(docsrs, feature(doc_cfg))]
-
-mod opcode;
-pub use opcode::*;
-
-pub mod operand;
-
-#[cfg_attr(docsrs, doc(cfg(feature = "sexpr")))]
-#[cfg(feature = "sexpr")]
-mod sexpr;
-
-#[cfg(feature = "sexpr")]
-pub use sexpr::*;
