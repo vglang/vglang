b14a90c3f1d9a1c3480253376d38a63b89363dc6
yayanyang <yayanyang@gmail.com>
2024-12-24 13:22:42 +0800

    sepxr implement with swiftui style

--- crates/sexpr/src/graphics.rs
+++ crates/sexpr/src/graphics.rs
@@ -1,4 +1,4 @@
-use vglang_opcode::el::Element;
+use vglang_opcode::el::{Element, Group, Text, TextSpan};
 use vglang_surface::Builder;
 
 /// The returns types of all sexprs must implement this trait.
@@ -10,12 +10,26 @@ where
     fn build(self, builder: &mut B);
 }
 
-impl<B, E> Graphics<B> for E
+impl<F, B> Graphics<B> for F
 where
-    E: Element,
     B: Builder,
+    F: FnOnce(&mut B),
 {
     fn build(self, builder: &mut B) {
-        builder.push(self.into_opcode());
+        self(builder)
     }
 }
+
+macro_rules! impl_graphics {
+    ($($name: ident),+) => {
+        $(impl<B> Graphics<B> for $name
+        where
+            B: Builder,{
+            fn build(self, builder: &mut B) {
+                builder.push(self.into_opcode())
+            }
+        })+
+    };
+}
+
+impl_graphics!(Text, TextSpan, Group);
--- crates/sexpr/tests/text.rs
+++ crates/sexpr/tests/text.rs
@@ -1,22 +1,26 @@
 mod sexpr;
 use sexpr::svg;
+
 use vglang_opcode::{
     attrs::{Fill, Stroke},
     data::Color,
     el::{Characters, Text, TextSpan},
 };
-use vglang_sexpr::*;
+use vglang_sexpr::{Graphics, Scontainer};
+use vglang_surface::Builder;
 
 #[futures_test::test]
 async fn test_text() {
-    svg("test", Text::from((0, 0))).await;
-    svg(
-        "test",
-        Text::from((0, 0))
-            .apply(Fill::from(Color::red))
-            .apply(Stroke::default())
-            .child(Characters::from("hello world"))
-            .child(TextSpan::default()),
-    )
-    .await;
+    svg("text_path", text_path()).await;
+}
+
+fn text_path<B>() -> impl Graphics<B>
+where
+    B: Builder,
+{
+    Text::from((0, 0))
+        .apply(Fill::from(Color::red))
+        .apply(Stroke::default())
+        .child(Characters::from("hello world"))
+        .child(TextSpan::default())
 }
