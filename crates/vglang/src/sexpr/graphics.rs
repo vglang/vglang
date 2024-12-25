e14779c722a9055999828b009d9e3e4190e3e7e5
yayanyang <yayanyang@gmail.com>
2024-12-25 20:51:46 +0800

    mark graphics

--- crates/vglang/src/sexpr/graphics.rs
+++ crates/vglang/src/sexpr/graphics.rs
@@ -1,23 +1,7 @@
-use crate::opcode::el::Container;
-
-use super::{BuildContext, ContentOf};
+use super::BuildContext;
 
 /// s-expr combinator must implement this trait.
 pub trait Graphics {
     /// Generate `opcode`s for specific surface.
     fn build(self, builder: &mut BuildContext);
-}
-
-pub trait Child<Target>: Graphics + ContentOf<Target>
-where
-    Container: From<Target>,
-{
-}
-
-impl<T, Target> Child<Target> for T
-where
-    T: ContentOf<Target> + Graphics,
-
-    Container: From<Target>,
-{
 }
