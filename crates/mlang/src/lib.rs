e747737247b454b87d4611fd462da97bef7d8409
yayanyang <yayanyang@gmail.com>
2025-01-08 21:53:43 +0800

    ml: add codegen feature

--- crates/mlang/src/lib.rs
+++ crates/mlang/src/lib.rs
@@ -1,7 +1,11 @@
 //! `mlang` is a programming language to define declarative programming language.
 
+#![cfg_attr(docsrs, feature(doc_cfg))]
+
 pub mod opcode;
 mod parser;
 pub use parser::*;
 mod semantic;
 pub use semantic::*;
+
+pub mod codegen;
