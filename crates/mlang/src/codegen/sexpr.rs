e6a8a84cdcef15ef2d9f2c5e84e2e155f9ca262f
yayanyang <yayanyang@gmail.com>
2025-01-11 23:30:31 +0800

    ml(codegen): sexpr impl default for nodes

--- crates/mlang/src/codegen/sexpr.rs
+++ crates/mlang/src/codegen/sexpr.rs
@@ -1,15 +1,66 @@
 use proc_macro2::TokenStream;
 use quote::quote;
 
-use crate::opcode::{ApplyTo, ChildrenOf, Enum, Node, Opcode};
+use crate::{
+    codegen::ext::{FieldGen, NodeGen},
+    opcode::{ApplyTo, ChildrenOf, Enum, Node, Opcode},
+};
 
 pub trait SexprGen {
     fn gen_sexpr_fns(&self, opcode_mod: &TokenStream) -> TokenStream;
 }
 
 impl SexprGen for Node {
-    fn gen_sexpr_fns(&self, _: &TokenStream) -> TokenStream {
-        quote! {}
+    fn gen_sexpr_fns(&self, opcode_mod: &TokenStream) -> TokenStream {
+        let mut fns = vec![];
+
+        fns.push(self.gen_sexpr_default_fn(opcode_mod));
+
+        quote! {
+            #(#fns)*
+        }
+    }
+}
+
+impl Node {
+    fn gen_sexpr_default_fn(&self, opcode_mod: &TokenStream) -> TokenStream {
+        let no_options = self.fields.iter().fold(0usize, |non_option, field| {
+            if field.is_option() {
+                non_option
+            } else {
+                non_option + 1
+            }
+        });
+
+        if no_options > 0 {
+            return quote! {};
+        }
+
+        let fields = self
+            .fields
+            .iter()
+            .map(|field| {
+                if let Some(ident) = field.gen_ident() {
+                    quote! { #ident: None}
+                } else {
+                    quote! { None }
+                }
+            })
+            .collect::<Vec<_>>();
+
+        let ident = self.gen_ident();
+
+        let ident = quote! {#opcode_mod #ident};
+
+        let body = self.gen_body_expr(fields);
+
+        quote! {
+            impl Default for #ident {
+                fn default() -> Self {
+                    Self #body
+                }
+            }
+        }
     }
 }
 
