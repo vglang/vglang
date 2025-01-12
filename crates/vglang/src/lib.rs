33c880986247f28faa7a7b9d08bdcf8f5784d44a
yayanyang <yayanyang@gmail.com>
2025-01-12 22:41:35 +0800

    remove sexpr

--- crates/vglang/src/lib.rs
+++ crates/vglang/src/lib.rs
@@ -1,13 +1,4 @@
 mod ml;
 pub use ml::opcode;
 
-#[cfg(feature = "sexpr")]
-mod manual;
-#[cfg(feature = "sexpr")]
-pub mod sexpr {
-
-    pub use super::manual::*;
-    pub use super::ml::sexpr::*;
-}
-
 pub use vglang_derive::rgb;
