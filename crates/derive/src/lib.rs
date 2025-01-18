b4c1df5beaa882cd31804647002c5a36c97f30c8
yayanyang <yayanyang@gmail.com>
2025-01-18 11:33:35 +0800

    derive: new arch init

--- crates/derive/src/lib.rs
+++ crates/derive/src/lib.rs
@@ -1,3 +1,8 @@
-//! `mlang` is a programming language to define declarative programming language.
+//! A code generation tool to help developing `vglang`.
 
 #![cfg_attr(docsrs, feature(doc_cfg))]
+
+pub mod analyzer;
+pub mod codegen;
+pub mod ir;
+pub mod parser;
--- crates/derive/tests/test_vglang.rs
+++ crates/derive/tests/test_vglang.rs
@@ -1,33 +1,33 @@
-use std::path::PathBuf;
+// use std::path::PathBuf;
 
-use parserc::{ParseContext, PrintReport};
-use vglang_derive::{codegen::gen, parse, semantic_analyze};
+// use parserc::{ParseContext, PrintReport};
+// use vglang_derive::{codegen::gen, parse, semantic_analyze};
 
-#[test]
-fn test_vglang() {
-    let mut input = ParseContext::from(include_str!("../../vglang/vglang.ml"));
+// #[test]
+// fn test_vglang() {
+//     let mut input = ParseContext::from(include_str!("../../vglang/vglang.ml"));
 
-    let mut opcodes = match parse(&mut input) {
-        Ok(opcodes) => opcodes,
-        Err(err) => {
-            input.report().print_reports();
-            panic!("{}", err);
-        }
-    };
+//     let mut opcodes = match parse(&mut input) {
+//         Ok(opcodes) => opcodes,
+//         Err(err) => {
+//             input.report().print_reports();
+//             panic!("{}", err);
+//         }
+//     };
 
-    if input.report_size() > 0 {
-        input.report().print_reports();
-        panic!("parase vglang.ml failed.");
-    }
+//     if input.report_size() > 0 {
+//         input.report().print_reports();
+//         panic!("parase vglang.ml failed.");
+//     }
 
-    semantic_analyze(&mut opcodes, &mut input);
+//     semantic_analyze(&mut opcodes, &mut input);
 
-    if input.report_size() > 0 {
-        input.report().print_reports();
-        panic!("vglang.ml semantic analyze failed.");
-    }
+//     if input.report_size() > 0 {
+//         input.report().print_reports();
+//         panic!("vglang.ml semantic analyze failed.");
+//     }
 
-    let ml = PathBuf::from(env!("CARGO_TARGET_TMPDIR")).join("ml.rs");
+//     let ml = PathBuf::from(env!("CARGO_TARGET_TMPDIR")).join("ml.rs");
 
-    gen(&opcodes, ml);
-}
+//     gen(&opcodes, ml);
+// }
